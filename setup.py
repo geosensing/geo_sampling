722d290756c9bcfd515b170b505f04d19c332c2b
Suriyan <suriyant@gmail.com>
2016-10-20 21:43:38 +0700

    update URL and bump version to 0.0.7

--- setup.py
+++ setup.py
@@ -59,13 +59,13 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.6',
+    version='0.0.7',
 
     description='Scripts for sampling Geo data sets by the specific region name',
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/suriyan/geo_sampling',
+    url='https://github.com/soodoku/geo_sampling',
 
     # Author details
     author='Suriyan Laohaprapanon, Gaurav Sood',
