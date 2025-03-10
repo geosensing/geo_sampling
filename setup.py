26f2aec98b679ccb4be37ad071f65a92356a3779
***** <gsood07@gmail.com>
2025-03-10 13:54:41 -0700

    move to python 3

--- setup.py
+++ setup.py
@@ -1,154 +1,85 @@
-"""A setuptools based setup module.
-See:
-https://packaging.python.org/en/latest/distributing.html
-https://github.com/pypa/sampleproject
-"""
-
-# Always prefer setuptools over distutils
 from setuptools import setup, find_packages
 from setuptools.command.develop import develop
 from setuptools.command.install import install
 from setuptools.command.test import test as TestCommand
+import codecs
+import os
 
-# To use a consistent encoding
-from codecs import open
-from os import path, system
-
-here = path.abspath(path.dirname(__file__))
+here = os.path.abspath(os.path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with codecs.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
-
 
 class PostDevelopCommand(develop):
     """Post-installation for development mode."""
     def run(self):
         print("TODO: PostDevelopCommand")
-        develop.run(self)
-
+        super().run()
 
 class PostInstallCommand(install):
     """Post-installation for installation mode."""
     def run(self):
         print("TODO: PostInstallCommand")
-        install.run(self)
-
+        super().run()
 
 class Tox(TestCommand):
     user_options = [('tox-args=', 'a', "Arguments to pass to tox")]
+    
     def initialize_options(self):
-        TestCommand.initialize_options(self)
+        super().initialize_options()
         self.tox_args = None
+
     def finalize_options(self):
-        TestCommand.finalize_options(self)
+        super().finalize_options()
         self.test_args = []
         self.test_suite = True
+
     def run_tests(self):
-        #import here, cause outside the eggs aren't loaded
         import tox
         import shlex
-        args = self.tox_args
-        if args:
-            args = shlex.split(self.tox_args)
+        args = shlex.split(self.tox_args) if self.tox_args else None
         tox.cmdline(args=args)
 
 setup(
     name='geo-sampling',
-
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.9',
-
+    version='0.1.0',  # Increment version for Python 3.10+
     description='Scripts for sampling Geo data sets by the specific region name',
     long_description=long_description,
-
-    # The project's main homepage.
+    long_description_content_type="text/x-rst",
     url='https://github.com/soodoku/geo_sampling',
-
-    # Author details
     author='Suriyan Laohaprapanon, Gaurav Sood',
     author_email='suriyant@gmail.com, gsood07@gmail.com',
-
-    # Choose your license
     license='MIT',
-
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
-
-        # Indicate who your project is intended for
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
-
-        # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: MIT License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2.7',
-
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Topic :: Scientific/Engineering :: Information Analysis',
         'Topic :: Scientific/Engineering :: GIS',
         'Topic :: Scientific/Engineering :: Visualization',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-
-    # What does your project relate to?
     keywords='geo street road openstreetmap osm city',
-
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
     packages=find_packages(exclude=['contrib', 'docs', 'tests', 'script']),
-
-    # Alternatively, if you want to distribute just a my_module.py, uncomment
-    # this:
-    #   py_modules=["my_module"],
-
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
+    python_requires=">=3.10",
     install_requires=[
-        'pyshp==1.2.3',
-        'matplotlib==1.5.1',
-        'pyproj==1.9.5.1',
-        'Shapely==1.4.0',
-        'utm==0.4.0',
-        'beautifulsoup4==4.5.1',
-        'requests==2.32.0'
+        'pyshp>=2.3.1',
+        'matplotlib>=3.5.0',
+        'pyproj>=3.3.0',
+        'Shapely>=2.0.0',
+        'utm>=0.7.0',
+        'beautifulsoup4>=4.10.0',
+        'requests>=2.32.0'
     ],
-
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). You can install these using the following syntax,
-    # for example:
-    # $ pip install -e .[dev,test]
     extras_require={
         'dev': ['check-manifest'],
         'test': ['coverage'],
     },
-
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
-    package_data={
-        'geo_sampling': [],
-    },
-
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
-    # In this case, 'geo_sampling' will be installed into '<sys.prefix>/geo_sampling'
-    
-    #data_files=[('geo_sampling', ['geo_sampling/data/test.txt'])],
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
+    package_data={'geo_sampling': []},
     entry_points={
         'console_scripts': [
             'geo_roads=geo_sampling.geo_roads:main',
